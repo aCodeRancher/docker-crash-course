a74473f569ab8f110415f124870a163834ae4423
helen <hhma@alumni.cpp.edu>
2020-05-24 20:06:56 -0400

    add @FeignClient (name="currency-exchange-service") to discover the service without specifying the hard code url add @EnableDiscoveryClient update pom to add eureka client dependencies update integration test as a SpringBootTest Update application.properties to make the shutdownEnableTimer return the IllegalStateException as a warning.

--- 05-microservices/currency-conversion-service/src/test/java/com/in28minutes/microservices/currencyconversionservice/resource/CurrencyConversionControllerTest.java
+++ 05-microservices/currency-conversion-service/src/test/java/com/in28minutes/microservices/currencyconversionservice/resource/CurrencyConversionControllerTest.java
@@ -1,41 +1,41 @@
 package com.in28minutes.microservices.currencyconversionservice.resource;
 
 import com.google.gson.Gson;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.junit.jupiter.api.Assertions.*;
 
-@WebMvcTest(CurrencyConversionController.class)
-@AutoConfigureDataJpa
+@SpringBootTest
 @AutoConfigureMockMvc
-//This auto configuration allows the test to configure CurrencyExchangeServiceProxy, which is a @FeignClient
-@ImportAutoConfiguration(FeignAutoConfiguration.class)
+
 class CurrencyConversionControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
     @Test
-    //Run CurrencyExchangeServiceApplication before running this test
+    //Run Eureka Naming Service, CurrencyExchangeServiceApplication before running this test
     public void convertCurrencyTest() throws Exception {
-        MvcResult result = mockMvc.perform(get("/currency-converter/from/USD/to/INR/quantity/100")).andReturn();
-        String currencyConversionBean_json = result.getResponse().getContentAsString();
-        assertTrue(currencyConversionBean_json!=null);
-        CurrencyConversionBean currencyConversionBean
-                   = new Gson().fromJson(currencyConversionBean_json, CurrencyConversionBean.class);
-        assertTrue(currencyConversionBean.getId()==10001);
-        assertTrue(currencyConversionBean.getFrom().equals("USD"));
-        assertTrue(currencyConversionBean.getTo().equals("INR"));
-        assertTrue(currencyConversionBean.getConversionMultiple().doubleValue()==  65.00);
-        assertTrue(currencyConversionBean.getTotalCalculatedAmount().doubleValue()== 6500.00);
+
+            MvcResult result = mockMvc.perform(get("/currency-converter/from/USD/to/INR/quantity/100")).andReturn();
+            String currencyConversionBean_json = result.getResponse().getContentAsString();
+            assertTrue(currencyConversionBean_json != null);
+            CurrencyConversionBean currencyConversionBean
+                    = new Gson().fromJson(currencyConversionBean_json, CurrencyConversionBean.class);
+            assertTrue(currencyConversionBean.getId() == 10001);
+            assertTrue(currencyConversionBean.getFrom().equals("USD"));
+            assertTrue(currencyConversionBean.getTo().equals("INR"));
+            assertTrue(currencyConversionBean.getConversionMultiple().doubleValue() == 65.00);
+            assertTrue(currencyConversionBean.getTotalCalculatedAmount().doubleValue() == 6500.00);
+
     }
+
+
 }
