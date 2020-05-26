f75393eaa2b9aa9964d30d7d3a2d82ef031135ea
helen <hhma@alumni.cpp.edu>
2020-05-26 16:29:39 -0400

    user zuul gateway, add ribbon dependency, add test to connect to the gateway

--- 05-microservices/currency-conversion-service/src/test/java/com/in28minutes/microservices/currencyconversionservice/resource/CurrencyConversionControllerTest.java
+++ 05-microservices/currency-conversion-service/src/test/java/com/in28minutes/microservices/currencyconversionservice/resource/CurrencyConversionControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -12,14 +14,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-
 class CurrencyConversionControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
     @Test
-    //Run Eureka Naming Service, CurrencyExchangeServiceApplication before running this test
+    //Run Eureka Naming Service, Zuul gateway,  CurrencyExchangeServiceApplication before running this test
     public void convertCurrencyTest() throws Exception {
 
             MvcResult result = mockMvc.perform(get("/currency-converter/from/USD/to/INR/quantity/100")).andReturn();
@@ -33,6 +34,20 @@ class CurrencyConversionControllerTest {
             assertTrue(currencyConversionBean.getConversionMultiple().doubleValue() == 65.00);
             assertTrue(currencyConversionBean.getTotalCalculatedAmount().doubleValue() == 6500.00);
 
+    }
+
+
+    @Test
+    //Run Eureka naming service, Zuul gateway, exchange service before running this test.
+    public void convertCurrency_zuul() {
+        TestRestTemplate testRestTemplate = new TestRestTemplate();
+        ResponseEntity<String> response = testRestTemplate.getForEntity("http://localhost:8765/currency-exchange-service/currency-exchange/from/USD/to/INR", String.class);
+          CurrencyConversionBean currencyConversionBean
+                = new Gson().fromJson(response.getBody(), CurrencyConversionBean.class);
+        assertTrue(currencyConversionBean.getId() == 10001);
+        assertTrue(currencyConversionBean.getFrom().equals("USD"));
+        assertTrue(currencyConversionBean.getTo().equals("INR"));
+        assertTrue(currencyConversionBean.getConversionMultiple().doubleValue() == 65.00);
     }
 
 
